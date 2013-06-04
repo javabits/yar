f018ce246e6166001be4fa42c3af75474c3bd897
Romain Gilles <rgilles@github>
2013-06-04 10:57:30 +0200

    #47: add test to validate registration of cleaner object on the registry

---
+++ yar-guice-osgi/src/test/java/org/javabits/yar/guice/osgi/internal/BundleTypeCleanerTest.java
@@ -0,0 +1,28 @@
+package org.javabits.yar.guice.osgi.internal;
+
+import org.javabits.yar.RegistryHook;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.verify;
+
+/**
+ * @author Romain Gilles
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class BundleTypeCleanerTest {
+    @Mock
+    RegistryHook registryHook;
+
+    @Test
+    public void testConstructorRegistration() {
+        //given registryHook
+        //when
+        BundleTypeCleaner bundleTypeCleaner = new BundleTypeCleaner(registryHook);
+        //then
+        verify(registryHook).addTypeListener(bundleTypeCleaner);
+    }
+}
